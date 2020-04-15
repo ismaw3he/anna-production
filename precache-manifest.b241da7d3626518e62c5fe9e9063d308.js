c6b12e9b683e49e7433c547c0ae261699aa79a60
Code_Father <rauf.raymond110@gmail.com>
2020-04-16 00:31:21 +0400

    first deploy test

---
+++ precache-manifest.b241da7d3626518e62c5fe9e9063d308.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9821046292d4bbbcf8a8e5f7ea42ffc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa119e81b4bbcfdb9a47",
+    "url": "/static/css/main.3892e54d.chunk.css"
+  },
+  {
+    "revision": "a0fa4417a62aae89e1d6",
+    "url": "/static/js/2.c94abe9b.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/static/js/2.c94abe9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa119e81b4bbcfdb9a47",
+    "url": "/static/js/main.4eb869ca.chunk.js"
+  },
+  {
+    "revision": "ca1479f632855f073e05",
+    "url": "/static/js/runtime-main.7eb8a470.js"
+  },
+  {
+    "revision": "f9e822d4ce1af0c083a801d4080e2564",
+    "url": "/static/media/pp.f9e822d4.jpg"
+  }
+]);
